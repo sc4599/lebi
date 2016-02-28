faf5f65c22e1dac14ed2a872cf8626874d4de468
suzaku <suzaku@suzaku.me>
2016-02-28 20:51:01 +0800

    增加分页

--- application/views/user/routes.py
+++ application/views/user/routes.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from flask import Blueprint, render_template, current_app, redirect, url_for
+from flask import Blueprint, render_template, current_app, redirect, url_for, request
 from .forms import SendLebiForm, PasswordEditForm
 from flask.ext.login import login_required, current_user
 from application.models.lebi import LeBi
@@ -11,12 +11,14 @@ user_blueprint = Blueprint('user', __name__)
 @user_blueprint.route('/')
 @login_required
 def index():
-    result = LeBi.objects().order_by('-created_at').all()
+    page = request.args.get('page', 1)
+    page = int(page)
+    result = LeBi.objects().order_by('-created_at').paginate(page=page, per_page=20)
     return render_template('user/index.html', result=result)
 
 
 @user_blueprint.route('/send/lebi/', methods=['GET', 'POST'])
-@user_blueprint.route('/send/lebi/<string:oid>/' , methods=['GET', 'POST'])
+@user_blueprint.route('/send/lebi/<string:oid>/', methods=['GET', 'POST'])
 @login_required
 def send_lebi(oid=None):
     if current_user.current_month_sent_count >= current_app.config.get('MONTH_LEBI_NUM', 10):
